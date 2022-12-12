29635043ac316686adc1e7b2cec4a0fca8f91af9
gggeek <giunta.gaetano@gmail.com>
2022-12-12 14:38:40 +0000

    clean up benchmark

--- tests/benchmark.php
+++ tests/benchmark.php
@@ -6,7 +6,7 @@ use PinbaPhp\Polyfill\PinbaFunctions as pinba;
 
 const ITERATIONS = 1000;
 const PAUSEUSECS = 0;
-$TAGS = array('t' => '1', 'longLongTag' => "This is possibly too long for its own good: lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+$TAGS = array('t' => '1'); //, 'longLongTag' => "This is possibly too long for its own good: lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
 
 function wait($usecs)
 {
